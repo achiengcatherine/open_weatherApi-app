000493452e54164ab29f4528be6673e6d37ec1fd
achiengcatherine <achiengcatherine431@gmail.com>
2023-06-26 09:27:46 +0300

    update file

--- weatherApi.py
+++ weatherApi.py
@@ -45,13 +45,13 @@ def getweather():
     print(json_data)
 
     #current
-    #wind = json_data['main']['wind'][0]['speed']
+    wind = json_data['wind']['speed']
     temp = json_data['main']['temp']
     humidity = json_data['main']['humidity']
     pressure = json_data['main']['pressure']
     desc = json_data['weather'][0]['description']
 
-   # w.config(text=(wind, "m/s"))
+    w.config(text=(wind, "m/s"))
     t.config(text=(temp, "°C"))
     h.config(text=(humidity, "%"))
     p.config(text=(pressure, "hpa"))
@@ -132,18 +132,18 @@ Label4 = Label(weather, text="Description", font=("Helvetica", 15, 'bold'), fg="
 Label4.place(x=60, y=310)
 
 #timezone
-timezone = Label(weather, font=("Helvetica", 20), fg="white" , bg="#142b44")
-timezone.place(x=700, y=50)
+timezone = Label(weather, font=("Helvetica", 20), fg="white", bg="#142b44")
+timezone.place(x=600, y=50)
 
 long_lat = Label(weather, font=("Helvetica", 10), fg="white", bg="#142b44")
-long_lat.place(x=700, y=90)
+long_lat.place(x=600, y=90)
 
 #clock
-clock = Label(weather, font=("Helvetica", 17, 'bold'), fg="white", bg="#72A0C1")
-clock.place(x=30, y=150)
+clock = Label(weather, font=("Helvetica", 17, 'bold'), fg="white", bg="#142b44")
+clock.place(x=600, y=130)
 
 #wthpd
-w = Label(weather, text="wind", font=("Helvetica", 11), fg="white", bg="#72A0C1")
+w = Label(weather, font=("Helvetica", 11), fg="white", bg="#72A0C1")
 w.place(x=200, y=230)
 t = Label(weather, font=("Helvetica", 11), fg="white", bg="#72A0C1")
 t.place(x=200, y=250)
@@ -155,7 +155,7 @@ d = Label(weather, font=("Helvetica", 11), fg="white", bg="#72A0C1")
 d.place(x=200, y=310)
 
 #first cell
-firstbox = Frame(weather, width=180, height=130, highlightthickness=1, bg="#72A0C1")
+firstbox = Frame(weather, width=180, height=130, highlightthickness=1, bg="#72a0c1")
 firstbox.place(x=20, y=450)
 
 day1 = Label(firstbox,text="day1", font=("arial", 20), bg="#72A0C1", fg="white")
@@ -165,61 +165,61 @@ firstImage = Label(firstbox, bg="#72A0C1")
 firstImage.place(x=4, y=15)
 
 #second cell
-secondbox = Frame(weather, width=100, height=130,highlightthickness=1,  bg="#72A0C1")
+secondbox = Frame(weather, width=110, height=130, highlightthickness=1,  bg="#72A0C1")
 secondbox.place(x=220, y=450)
 
-day2 = Label(secondbox, font=("arial",20), bg="#72A0C1", fg="white")
+day2 = Label(secondbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day2.place(x=10, y=15)
 
 secondImage = Label(secondbox, bg="#72A0C1")
 secondImage.place(x=4, y=15)
 
 #third cell
-thirdbox = Frame(weather, width=100, height=130,highlightthickness=1, bg="#72A0C1")
-thirdbox.place(x=330, y=450)
+thirdbox = Frame(weather, width=120, height=130, highlightthickness=1, bg="#72A0C1")
+thirdbox.place(x=350, y=450)
 
 thirdImage = Label(thirdbox, bg="#72A0C1")
 thirdImage.place(x=4, y=15)
 
-day3 = Label(thirdbox, font=("arial",20), bg="#72A0C1", fg="white")
+day3 = Label(thirdbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day3.place(x=10, y=15)
 
 
 #fourth cell
-fourthbox = Frame(weather, width=100, height=130,highlightthickness=1, bg="#72A0C1")
-fourthbox.place(x=440, y=450)
+fourthbox = Frame(weather, width=110, height=130, highlightthickness=1, bg="#72A0C1")
+fourthbox.place(x=490, y=450)
 
-day4 = Label(fourthbox, font=("arial",20), bg="#72A0C1", fg="white")
+day4 = Label(fourthbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day4.place(x=10, y=15)
 
 fourthImage = Label(fourthbox, bg="#72A0C1")
 fourthImage.place(x=4, y=15)
 
 #fifth cell
-fifthbox = Frame(weather, width=100, height=130, highlightthickness=1, bg="#72A0C1")
-fifthbox.place(x=550, y=450)
+fifthbox = Frame(weather, width=110, height=130, highlightthickness=1, bg="#72A0C1")
+fifthbox.place(x=620, y=450)
 
-day5 = Label(fifthbox, font=("arial",20), bg="#72A0C1", fg="white")
+day5 = Label(fifthbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day5.place(x=10, y=15)
 
 fifthImage = Label(fifthbox, bg="#72A0C1")
 fifthImage.place(x=4, y=15)
 
 #sixth cell
-sixthbox = Frame(weather, width=100, height=130, highlightthickness=1, bg="#72A0C1")
-sixthbox.place(x=660, y=450)
+sixthbox = Frame(weather, width=110, height=130, highlightthickness=1, bg="#72A0C1")
+sixthbox.place(x=750, y=450)
 
-day6 = Label(sixthbox, font=("arial",20), bg="#72A0C1", fg="white")
+day6 = Label(sixthbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day6.place(x=10, y=15)
 
 sixthImage = Label(sixthbox, bg="#72A0C1")
 sixthImage.place(x=4, y=15)
 
 #seventh cell
-seventhbox = Frame(weather, width=100, height=130, highlightthickness=1,highlightcolor="red", background="#0093AF", bg="#72A0C1")
-seventhbox.place(x=770, y=450)
+seventhbox = Frame(weather, width=110, height=130, highlightthickness=1, highlightcolor="red", background="#0093AF", bg="#72A0C1")
+seventhbox.place(x=880, y=450)
 
-day7 = Label(seventhbox, font=("arial",20), bg="#72A0C1", fg="white")
+day7 = Label(seventhbox, font=("arial", 20), bg="#72A0C1", fg="white")
 day7.place(x=10, y=15)
 
 seventhImage = Label(seventhbox, bg="#72A0C1")
